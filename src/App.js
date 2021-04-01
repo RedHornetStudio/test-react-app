700d95db18b20988fe5c1f92f599f91b5c14b0b1
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-04-01 14:12:48 +0300

    new file:   .gitignore new file:   README.md new file:   package-lock.json new file:   package.json new file:   public/favicon.ico new file:   public/index.html new file:   public/logo192.png new file:   public/logo512.png new file:   public/manifest.json new file:   public/robots.txt new file:   src/App.css new file:   src/App.js new file:   src/App.test.js new file:   src/index.css new file:   src/index.js new file:   src/logo.svg new file:   src/reportWebVitals.js new file:   src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>I am a React app</div>
+    );
+  }
+}
+
+export default App;
